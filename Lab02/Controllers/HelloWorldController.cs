c8db91a510ccc9f190d92ec07f114df96c52a404
Alucard30Dec <alucard30dec@gmail.com>
2025-12-01 17:27:56 +0700

    Update lab

---
+++ Lab02/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return Content("This is my default action...");
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome()
+        {
+            return Content("This is the Welcome action method...");
+        }
+    }
+}
